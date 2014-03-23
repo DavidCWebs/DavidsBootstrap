c104833c4e76b3b4c7fe7c0b03e68c8061af83c2
DavidCWebs <david@carawebs.com>
2014-03-23 20:45:53 +0000

    new file:   carawebs-midwest-firewood/css/bootstrap-lightbox.min.css 	new file:   carawebs-midwest-firewood/css/bootstrap.css 	new file:   carawebs-midwest-firewood/css/bootstrap.min.css 	new file:   carawebs-midwest-firewood/css/grayscale.css~ 	new file:   carawebs-midwest-firewood/css/midwest-firewood.css 	new file:   carawebs-midwest-firewood/css/midwest-firewood.css~ 	new file:   carawebs-midwest-firewood/font-awesome/css/font-awesome.css 	new file:   carawebs-midwest-firewood/font-awesome/css/font-awesome.min.css 	new file:   carawebs-midwest-firewood/font-awesome/fonts/FontAwesome.otf 	new file:   carawebs-midwest-firewood/font-awesome/fonts/fontawesome-webfont.eot 	new file:   carawebs-midwest-firewood/font-awesome/fonts/fontawesome-webfont.svg 	new file:   carawebs-midwest-firewood/font-awesome/fonts/fontawesome-webfont.ttf

---
+++ carawebs-midwest-firewood/js/grayscale.js
@@ -0,0 +1,163 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// mobile nav toggle - close the menu when an item is clicked
+	$('.nav a').on('click', function(){
+	if ($(document).width() <= 767){
+	$(".navbar-toggle").click();
+	}
+
+});
+
+// Back to Top Navigation
+	
+	// Show or hide the sticky footer button
+			$(window).scroll(function() {
+				if ($(this).scrollTop() > 200) {
+					$('.go_top').fadeIn(200);
+				} else {
+					$('.go_top').fadeOut(200);
+				}
+			});
+	
+	// Animate the scroll to top
+			$('.go_top').click(function(event) {
+				event.preventDefault();
+				
+				$('html, body').animate({scrollTop: 0}, 300);
+			})	
+
+//Google Map Skin - Get more at http://snazzymaps.com/
+var myOptions = {
+    zoom: 15,
+    center: new google.maps.LatLng(53.385873, -1.471471),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    disableDefaultUI: true,
+    styles: [{
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#000000"
+        }, {
+            "lightness": 17
+        }]
+    }, {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#000000"
+        }, {
+            "lightness": 20
+        }]
+    }, {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "color": "#000000"
+        }, {
+            "lightness": 17
+        }]
+    }, {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+            "color": "#000000"
+        }, {
+            "lightness": 29
+        }, {
+            "weight": 0.2
+        }]
+    }, {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#000000"
+        }, {
+            "lightness": 18
+        }]
+    }, {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#000000"
+        }, {
+            "lightness": 16
+        }]
+    }, {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#000000"
+        }, {
+            "lightness": 21
+        }]
+    }, {
+        "elementType": "labels.text.stroke",
+        "stylers": [{
+            "visibility": "on"
+        }, {
+            "color": "#000000"
+        }, {
+            "lightness": 16
+        }]
+    }, {
+        "elementType": "labels.text.fill",
+        "stylers": [{
+            "saturation": 36
+        }, {
+            "color": "#000000"
+        }, {
+            "lightness": 40
+        }]
+    }, {
+        "elementType": "labels.icon",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [{
+            "color": "#000000"
+        }, {
+            "lightness": 19
+        }]
+    }, {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "color": "#000000"
+        }, {
+            "lightness": 20
+        }]
+    }, {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+            "color": "#000000"
+        }, {
+            "lightness": 17
+        }, {
+            "weight": 1.2
+        }]
+    }]
+};
+
+var map = new google.maps.Map(document.getElementById('map'), myOptions);
